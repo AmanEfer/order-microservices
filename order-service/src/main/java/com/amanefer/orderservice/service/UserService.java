9c726f74668de137485e7dede387c35349571fa4
Alex Pripisnov <a.filtsov@gmail.com>
2026-04-22 13:07:48 +0300

    change UserService from interface to class

--- order-service/src/main/java/com/amanefer/orderservice/service/UserService.java
+++ order-service/src/main/java/com/amanefer/orderservice/service/UserService.java
@@ -1,17 +1,58 @@
 package com.amanefer.orderservice.service;
 
+import com.amanefer.orderservice.exception.EntityNotFoundException;
 import com.amanefer.orderservice.model.dto.UserRequest;
 import com.amanefer.orderservice.model.entity.User;
+import com.amanefer.orderservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
-public interface UserService {
+@Service
+@RequiredArgsConstructor
+public class UserService {
 
-    User createNewUser(UserRequest userRequest);
+    @Value("${user.role.default-name}")
+    private String defaultRoleName;
 
-    List<User> getAllUsers();
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
-    User getUserById(Long id);
+    @Transactional
+    public User createNewUser(UserRequest request) {
+        var role = roleService.getRoleByName(defaultRoleName);
 
-    String deleteUserById(Long id);
+        var user = User.builder()
+                .username(request.username())
+                .password(passwordEncoder.encode(request.password()))
+                .email(request.email())
+                .roles(Set.of(role))
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Пользователь с ID " + id + " не найден"));
+    }
+
+    @Transactional
+    public String deleteUserById(Long id) {
+        var userForDelete = getUserById(id);
+        userRepository.delete(userForDelete);
+
+        return "Пользователь с ID " + id + " успешно удален";
+    }
 }
